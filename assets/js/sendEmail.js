57d8aec2ca238c602fe696a109191f0d3cada037
Ioan Hadarean <hadareannelutu@yahoo.com>
2018-10-23 15:42:50 +0000

    Fixed reloading page issue

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,14 +1,15 @@
 function sendMail(form) {
-    emailjs.send("gmail", "rosie", {
-        "form_name": form.name.value,
-        "form_email": form.emailaddress.value,
-        "project_request": form.projectsummary.value
-    })
-    .then (
-        function(response){
-           console.log("SUCCESS", response) ;
-        },
-        function(error){
-           console.log("FAILED", error);
-        });
+    emailjs.send("gmail", "jack", {
+            "from_name": form.name.value,
+            "from_email": form.emailaddress.value,
+            "project_request": form.projectsummary.value
+        })
+        .then(
+            function(response) {
+                console.log("SUCCESS", response);
+            },
+            function(error) {
+                console.log("FAILED", error);
+            });
+        return false;
 }
